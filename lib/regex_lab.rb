fca826905b03d05f51963f03298a336e42a58488
Ricardo Paul <kingupper0@gmail.com>
2019-04-10 15:28:00 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+   if word.match(/^[aeiouAEIOU]/) then
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/un+\w+ing\b/)
 end
 
+# \b and \b delimits the word length
 def words_five_letters_long(text)
-
+ text.scan(/\b\w{5}\b/)
 end
 
+# .* matches any character ( 0 or more)
+# .*\W any non-word and any character
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ text.match(/^[A-Z].*\W$/)? true : false
 end
+# /^[A-Z].+[\.!?]$
+
 
 def valid_phone_number?(phone)
-
-end
+   phone.scan(/\d/).count == 10
+end	end
