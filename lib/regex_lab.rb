1f2c0df7d444ee4b338a842cf9b4b08e69711a8e
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-12 17:12:04 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
 def starts_with_a_vowel?(word)
+if word.match(/^[aeiou]|^[AEIOU]/)
+ return true
+ else
+   return false
+ end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
-end
+  text.scan(/un\w*ing/)
+  end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    beg_number = text.scan(/\w+/)[0]
+  if (text.scan(/\./)) && (beg_number == beg_number.upcase)
+  return  true
+  else
+  return  false
+  end
 end
 
 def valid_phone_number?(phone)
-
+ if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+  return  true
+  else
+  return  false
+  end
 end
